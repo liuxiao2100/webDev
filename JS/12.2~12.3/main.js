7230f133c79df482cfd29cc87af990c415b0f780
liuxiao2100 <liuxiao2100@foxmail.com>
2018-07-31 17:30:46 +0800

    增加学习过程中文件
    对照表严肃进行学习

---
+++ JS/12.2~12.3/main.js
@@ -0,0 +1,48 @@
+var 姓名 = 'wanghuahua';
+alert('你好， ' + 姓名);
+// Number
+123
+1.2
+-1
+NaN
+Infinity //无限大
+1e3
+1.2e4
+
+//String 字符串
+'王花花';
+"王花花";
+'he\'s';
+'啊，祖国'
+`啊
+祖国`
+
+// Boolean 布尔值
+true
+false
+0
+1
+
+// Array 数组
+[
+  'hao',
+  'nihao',
+  'dajiahao',
+  1,
+  {a:b}
+]
+
+// Object 对象
+[
+  {
+    title: '号',
+    content: 'lalala'
+  }
+]
+
+var result = {
+  title: '胸疼是什么原因',
+  desc: '病情分析'
+}
+
+result.desc
---
+++ JS/12.4/main.js
@@ -0,0 +1,27 @@
+// var name = 'Yo';
+// name = 1;
+// // 一个变量在一个点上只能存一个值
+// var bag = true;
+// bag = []; // Object
+// bag = {};
+// console.log(name);
+// console.log(typeof bag);
+//
+// let bag = 'Yo';
+
+// // 12.5
+// let isAdmin = true;
+// if(isAdmin) {
+//   console.log('他是管理员');
+// } else {
+//   console.log('他不是管理员');
+// }
+
+// 12.6
+let day = 4;
+switch (day) {
+  case 1: console.log('今天吃牛肉面');break;
+  case 2: console.log('今天吃肉筋饭');break;
+  case 3: console.log('今天不回家');break;
+  default: console.log('食屎啦');
+}
